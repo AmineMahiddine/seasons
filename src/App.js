17560921e458fde547d8f8d57cd35e21e44a3294
Mohamed Amine Mahiddine <aminemahiddine.io@gmail.com>
2022-08-18 17:29:13 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// const App = () => {
+
+// window.navigator.geolocation.getCurrentPosition(
+//   (position) => console.log(`my position ${position.coords.longitude}`),
+//   // (err)=> console.log(err)
+// );
+
+//   return (
+//     <div className="App">
+//     </div>
+//   );
+// };
+
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = { lat: null, errorMessage: '' };
+
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({ lat: position.coords.latitude })
+      },
+      (err) => {
+        this.setState({ errorMessage: err.message })
+      }
+    );
+  }
+
+
+  render() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <h1>ErrorMessage: {this.state.errorMessage}</h1>
+    }
+    if (!this.state.errorMessage && this.state.lat) {
+      return <h1>Lattitude: {this.state.lat}</h1>
+    }
+    return (
+      <div>
+        Loading...
+      </div>
+    )
+  }
 }
 
 export default App;
